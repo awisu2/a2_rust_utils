9c67edd40326d22390eea5c295cd774c4d9e1dff
awisu2 <awisu2@gmail.com>
2024-03-10 10:40:28 +0900

    update

--- src/path.rs
+++ src/path.rs
@@ -1,7 +1,6 @@
 use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 
-#[allow(dead_code)]
 pub fn osstr_to_string(osstr: &OsStr, def: &str) -> String {
     match osstr.to_os_string().to_str() {
         Some(v) => String::from(v),
@@ -9,7 +8,6 @@ pub fn osstr_to_string(osstr: &OsStr, def: &str) -> String {
     }
 }
 
-#[allow(dead_code)]
 pub fn opt_osstr_to_string(opt_osstr: Option<&OsStr>, def: &str) -> String {
     let v = match opt_osstr {
         Some(v) => v,
@@ -18,12 +16,10 @@ pub fn opt_osstr_to_string(opt_osstr: Option<&OsStr>, def: &str) -> String {
     osstr_to_string(v, def)
 }
 
-#[allow(dead_code)]
 pub fn pathbuf_to_string(pathbuf: PathBuf, def: &str) -> String {
     osstr_to_string(pathbuf.as_os_str(), def)
 }
 
-#[allow(dead_code)]
 pub fn path_to_string(path: &Path, def: &str) -> String {
     path.to_str()
         .map(|v| String::from(v))
