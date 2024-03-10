9741ecf9590a4258e465d28049dff021bf2efc8e
awisu2 <awisu2@gmail.com>
2024-03-10 10:00:43 +0900

    modify files

--- src/files.rs
+++ src/files.rs
@@ -1,77 +1,125 @@
-mod config;
-mod file;
-mod filemeta;
-mod os;
-mod path;
-mod stores;
-mod thumbnail;
+use serde::{Deserialize, Serialize};
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::time::{SystemTime, UNIX_EPOCH};
 
-use std::path::PathBuf;
+use crate::commands::path;
 
-use self::{file::FileInfo, filemeta::FileMeta};
+#[derive(Serialize, Deserialize, Debug)]
+pub struct FileInfo {
+    pub path: PathBuf,
+    pub file_name: String,
+    pub extension: String,
+    pub is_dir: bool,
+    pub is_file: bool,
+    pub is_image: bool,
+    pub is_movie: bool,
 
-// config =====
-#[tauri::command]
-pub fn write_config(app: tauri::AppHandle, config: config::Config) -> Result<(), String> {
-    config::write(app, config)
+    pub modified: u64,
+    pub created: u64,
+
+    pub parent_path: PathBuf,
 }
 
-#[tauri::command]
-pub fn read_config(app: tauri::AppHandle) -> Result<config::Config, String> {
-    config::read(app)
+pub fn read_dir(dir: &str) -> Result<Vec<FileInfo>, String> {
+    let mut vec: Vec<FileInfo> = Vec::new();
+
+    let path = Path::new(dir);
+    if !path.is_dir() {
+        return Err(format!("not directory."));
+    }
+
+    let read_dir = match fs::read_dir(path) {
+        Ok(v) => v,
+        Err(e) => return Err(e.to_string()),
+    };
+    for entry in read_dir {
+        let entry = match entry {
+            Ok(v) => v,
+            Err(e) => return Err(e.to_string()),
+        };
+        let file_info = pathbuf_to_fileinfo(entry.path());
+        vec.push(file_info);
+    }
+
+    Ok(vec)
 }
 
-#[tauri::command]
-pub fn get_config_path(app: tauri::AppHandle) -> Result<PathBuf, String> {
-    match config::get_config_path(app) {
-        Some(path) => Ok(path),
-        None => Err(String::from("no config_path")),
+pub fn str_to_fileinfo(str: &str) -> FileInfo {
+    let pathbuf = PathBuf::from(str);
+    pathbuf_to_fileinfo(pathbuf)
+}
+
+pub fn is_exists_file(path: &str) -> bool {
+    let pathbuf = PathBuf::from(path);
+    pathbuf.exists()
+}
+
+fn pathbuf_to_fileinfo(pathbuf: PathBuf) -> FileInfo {
+    let _pathbuf = pathbuf.clone();
+    let path = _pathbuf.as_path();
+    let file_name = path::opt_osstr_to_string(path.file_name(), "");
+    let extension = path::opt_osstr_to_string(path.extension(), "");
+
+    let is_dir = path.is_dir();
+    let is_movie = is_movie(extension.as_str());
+    let is_image = is_image(extension.as_str());
+
+    let (modified, created) = match pathbuf.metadata() {
+        Ok(v) => (
+            system_time_to_u64(v.modified().unwrap()),
+            system_time_to_u64(v.created().unwrap()),
+        ),
+        Err(_) => (0, 0),
+    };
+    let parnt_path = pathbuf.parent().unwrap().to_path_buf();
+
+    FileInfo {
+        path: pathbuf,
+        file_name,
+        extension: extension,
+        is_dir: is_dir,
+        is_file: path.is_file(),
+        is_movie: is_movie,
+        is_image: is_image,
+        modified: modified,
+        created: created,
+        parent_path: parnt_path,
     }
 }
 
-// files =====
-#[tauri::command]
-pub fn read_dir(dir: &str) -> Result<Vec<file::FileInfo>, String> {
-    file::read_dir(dir)
+fn is_movie(extension: &str) -> bool {
+    return extension == "mp4"
+        || extension == "mpeg"
+        || extension == "mpg"
+        || extension == "avi"
+        || extension == "mov";
 }
 
-#[tauri::command]
-pub fn get_fileinfo(path: &str) -> file::FileInfo {
-    file::str_to_fileinfo(path)
+fn is_image(extension: &str) -> bool {
+    return extension == "jpeg"
+        || extension == "jpg"
+        || extension == "gif"
+        || extension == "webp"
+        || extension == "png";
 }
 
-#[tauri::command]
-pub fn is_exists_file(path: &str) -> bool {
-    file::is_exists_file(path)
+fn system_time_to_u64(system_time: SystemTime) -> u64 {
+    let duration = match system_time.duration_since(UNIX_EPOCH) {
+        Ok(v) => v,
+        Err(_) => return 0,
+    };
+    duration.as_secs()
 }
 
-// favorites =====
-#[tauri::command]
-pub fn read_filemetas(app: tauri::AppHandle) -> Result<Vec<FileMeta>, String> {
-    filemeta::read(app)
-}
-
-#[tauri::command]
-pub fn write_filemetas(app: tauri::AppHandle, favorites: Vec<FileMeta>) -> Result<(), String> {
-    filemeta::write(app, favorites)
-}
-
-#[tauri::command]
-pub fn get_thumbnailable_path(dir: &str, deep: i8) -> Result<FileInfo, String> {
-    thumbnail::get_thumbnailable_path(dir, deep)
-}
-
-#[tauri::command]
-pub fn open_filer(dir: &str) -> Result<(), String> {
-    os::open_filer(dir)
-}
-
-#[tauri::command]
 pub fn rename(from: &str, to: &str) -> Result<(), String> {
-    file::rename(from, to)
+    match fs::rename(from, to) {
+        Ok(_) => (),
+        Err(e) => return Err(e.to_string()),
+    }
+    Ok(())
 }
 
-#[tauri::command]
 pub fn remove_dir_all(path: &str) -> Result<(), String> {
-    file::remove_dir_all(path)
+    fs::remove_dir_all(path).map_err(|e| e.to_string())
 }
