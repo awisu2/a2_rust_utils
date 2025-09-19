91f8822eb64dc032997f711b3299c4407a213b13
awisu2 <awisu2@gmail.com>
2025-09-20 08:06:07 +0900

    modify dir functions to Result

--- src/tauri/mod.rs
+++ src/tauri/mod.rs
@@ -1,14 +1,15 @@
+use anyhow::{Ok, Result};
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 use tauri::Manager;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct AppDirs {
-    pub config_dir: PathBuf,
-    pub data_dir: PathBuf,
-    pub cache_dir: PathBuf,
-    pub log_dir: PathBuf,
-    pub local_data_dir: PathBuf,
+    pub config_dir: PathBuf,     // App configuration directory
+    pub cache_dir: PathBuf,      // App cache directory
+    pub log_dir: PathBuf,        // App log directory
+    pub local_data_dir: PathBuf, // data directory.
+    pub data_dir: PathBuf, // data directory. roaming on Windows (roaming is only windows concept)
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -30,36 +31,36 @@ pub struct SystemDirs {
     pub temp_dir: PathBuf,
 }
 
-pub fn app_dirs(handle: tauri::AppHandle) -> AppDirs {
+pub fn app_dirs(handle: tauri::AppHandle) -> Result<AppDirs> {
     let path = handle.path();
 
-    AppDirs {
-        config_dir: path.app_config_dir().unwrap_or_default(),
-        data_dir: path.app_data_dir().unwrap_or_default(),
-        cache_dir: path.app_cache_dir().unwrap_or_default(),
-        log_dir: path.app_log_dir().unwrap_or_default(),
-        local_data_dir: path.app_local_data_dir().unwrap_or_default(),
-    }
+    Ok(AppDirs {
+        config_dir: path.app_config_dir()?,
+        data_dir: path.app_data_dir()?,
+        cache_dir: path.app_cache_dir()?,
+        log_dir: path.app_log_dir()?,
+        local_data_dir: path.app_local_data_dir()?,
+    })
 }
 
-pub fn system_dirs(handle: tauri::AppHandle) -> SystemDirs {
+pub fn system_dirs(handle: tauri::AppHandle) -> Result<SystemDirs> {
     let path = handle.path();
 
-    SystemDirs {
-        config_dir: path.config_dir().unwrap_or_default(),
-        data_dir: path.data_dir().unwrap_or_default(),
-        local_data_dir: path.local_data_dir().unwrap_or_default(),
-        desktop_dir: path.desktop_dir().unwrap_or_default(),
-        document_dir: path.document_dir().unwrap_or_default(),
-        download_dir: path.download_dir().unwrap_or_default(),
-        executable_dir: path.executable_dir().unwrap_or_default(),
-        font_dir: path.font_dir().unwrap_or_default(),
-        home_dir: path.home_dir().unwrap_or_default(),
-        picture_dir: path.picture_dir().unwrap_or_default(),
-        runtime_dir: path.runtime_dir().unwrap_or_default(),
-        template_dir: path.template_dir().unwrap_or_default(),
-        video_dir: path.video_dir().unwrap_or_default(),
-        resource_dir: path.resource_dir().unwrap_or_default(),
-        temp_dir: path.temp_dir().unwrap_or_default(),
-    }
+    Ok(SystemDirs {
+        config_dir: path.config_dir()?,
+        data_dir: path.data_dir()?,
+        local_data_dir: path.local_data_dir()?,
+        desktop_dir: path.desktop_dir()?,
+        document_dir: path.document_dir()?,
+        download_dir: path.download_dir()?,
+        executable_dir: path.executable_dir()?,
+        font_dir: path.font_dir()?,
+        home_dir: path.home_dir()?,
+        picture_dir: path.picture_dir()?,
+        runtime_dir: path.runtime_dir()?,
+        template_dir: path.template_dir()?,
+        video_dir: path.video_dir()?,
+        resource_dir: path.resource_dir()?,
+        temp_dir: path.temp_dir()?,
+    })
 }
