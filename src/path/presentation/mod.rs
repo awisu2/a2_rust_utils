2690a0134dea5923b3a52738bda55e877f14ac2e
awisu2 <awisu2@gmail.com>
2025-12-23 12:45:03 +0900

    update osstr to string

--- src/path/presentation/mod.rs
+++ src/path/presentation/mod.rs
@@ -1,5 +1,14 @@
-use std::path::{Path, PathBuf};
+use std::{ffi::OsStr, path::Path};
 
+// convert &Path to String.
+// Pathbuf also can use this function
 pub fn path_to_string(path: &Path) -> String {
     path.to_string_lossy().into_owned()
+}
+
+// convert Option<&OsStr> to String
+// this is for path.extension() and path.file_name()
+pub fn osstr_opt_to_string(str: Option<&OsStr>) -> String {
+    str.map(|n| n.to_string_lossy().into_owned())
+        .unwrap_or_default()
 }
