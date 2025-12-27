f69852e6b79357279141092d83f1b2930e5354f2
awisu2 <awisu2@gmail.com>
2025-12-27 14:29:02 +0900

    add rwlock

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@ pub mod hello;
 pub mod images;
 pub mod os;
 pub mod path;
+pub mod rwlock;
 pub mod sqlite;
 pub mod tauri;
---
+++ src/rwlock.rs
@@ -0,0 +1,48 @@
+// Rwlock の拡張 lock時のエラーハンドリングの煩雑さを解消するためのユーティリティ
+use anyhow::{anyhow, Result};
+use std::sync::RwLock;
+
+pub struct A2RwLock<T> {
+    inner: RwLock<Option<T>>,
+}
+
+impl<T: Clone> A2RwLock<T> {
+    pub fn new() -> Self {
+        Self {
+            inner: RwLock::new(None),
+        }
+    }
+
+    fn type_name(&self) -> &'static str {
+        std::any::type_name::<T>()
+    }
+
+    pub fn set(&self, value: T) -> Result<()> {
+        let mut w_guard = self
+            .inner
+            .write()
+            .map_err(|e| anyhow!("Failed to {} write lock. {}", self.type_name(), e))?;
+
+        *w_guard = Some(value);
+        Ok(())
+    }
+
+    pub fn get(&self) -> Result<T> {
+        let r_guard = self
+            .inner
+            .read()
+            .map_err(|e| anyhow!("Failed to {} read lock. {}", self.type_name(), e))?;
+
+        r_guard.clone().ok_or_else(|| anyhow!("Value not set"))
+    }
+
+    pub fn with<R>(&self, f: impl FnOnce(&T) -> R) -> Result<R> {
+        let r_guard = self
+            .inner
+            .read()
+            .map_err(|e| anyhow!("Failed to {} read lock. {}", self.type_name(), e))?;
+
+        let value = r_guard.as_ref().ok_or_else(|| anyhow!("Value not set"))?;
+        Ok(f(value))
+    }
+}
