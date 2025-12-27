bf245c25759dc170e0b0d991f719b7ec167e72ba
awisu2 <awisu2@gmail.com>
2025-12-27 14:55:41 +0900

    fix error log

--- src/rwlock.rs
+++ src/rwlock.rs
@@ -1,7 +1,7 @@
-// Rwlock の拡張 lock時のエラーハンドリングの煩雑さを解消するためのユーティリティ
 use anyhow::{anyhow, Result};
 use std::sync::RwLock;
 
+// Rwlock の拡張 lock時のエラーハンドリングの煩雑さを解消するためのユーティリティ
 pub struct A2RwOptionLock<T> {
     inner: RwLock<Option<T>>,
 }
@@ -17,32 +17,41 @@ impl<T: Clone> A2RwOptionLock<T> {
         std::any::type_name::<T>()
     }
 
-    pub fn set(&self, value: T) -> Result<()> {
-        let mut w_guard = self
+    fn get_read_guard(&self) -> Result<std::sync::RwLockReadGuard<'_, Option<T>>> {
+        let r_guard = self
+            .inner
+            .read()
+            .map_err(|e| anyhow!("Failed to read lock {}. {}", self.type_name(), e))?;
+        Ok(r_guard)
+    }
+
+    fn get_write_guard(&self) -> Result<std::sync::RwLockWriteGuard<'_, Option<T>>> {
+        let w_guard = self
             .inner
             .write()
-            .map_err(|e| anyhow!("Failed to {} write lock. {}", self.type_name(), e))?;
+            .map_err(|e| anyhow!("Failed to write lock {}. {}", self.type_name(), e))?;
+        Ok(w_guard)
+    }
 
+    pub fn set(&self, value: T) -> Result<()> {
+        let mut w_guard = self.get_write_guard()?;
         *w_guard = Some(value);
+
         Ok(())
     }
 
     pub fn get(&self) -> Result<T> {
-        let r_guard = self
-            .inner
-            .read()
-            .map_err(|e| anyhow!("Failed to {} read lock. {}", self.type_name(), e))?;
-
-        r_guard.clone().ok_or_else(|| anyhow!("Value not set"))
+        self.get_read_guard()?
+            .clone()
+            .ok_or_else(|| anyhow!("{} not setted", self.type_name()))
     }
 
     pub fn with<R>(&self, f: impl FnOnce(&T) -> R) -> Result<R> {
-        let r_guard = self
-            .inner
-            .read()
-            .map_err(|e| anyhow!("Failed to {} read lock. {}", self.type_name(), e))?;
+        let r_guard = self.get_read_guard()?;
+        let value = r_guard
+            .as_ref()
+            .ok_or_else(|| anyhow!("{} not setted", self.type_name()))?;
 
-        let value = r_guard.as_ref().ok_or_else(|| anyhow!("Value not set"))?;
         Ok(f(value))
     }
 }
