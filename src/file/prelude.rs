a1859e41c4f87b90c6fddb082742d02a43529341
awisu2 <awisu2@gmail.com>
2026-02-14 13:53:40 +0900

    set prefulet PathEx

--- src/file/prelude.rs
+++ src/file/prelude.rs
@@ -3,3 +3,4 @@ pub use crate::file::application::*;
 pub use crate::file::domain::file_entry::*;
 pub use crate::file::domain::file_info::*;
 pub use crate::file::domain::file_meta::*;
+pub use crate::file::path_ex::PathEx;
