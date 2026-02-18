6b7909c786cc0711a78ef363040d46241133452c
awisu2 <awisu2@gmail.com>
2026-02-18 20:17:49 +0900

    update

--- src/file/prelude.rs
+++ src/file/prelude.rs
@@ -3,4 +3,5 @@ pub use crate::file::application::*;
 pub use crate::file::domain::file_entry::*;
 pub use crate::file::domain::file_info::*;
 pub use crate::file::domain::file_meta::*;
+pub use crate::file::domain::zip_infos::*;
 pub use crate::file::path_ex::*;
